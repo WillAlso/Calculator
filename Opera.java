9c6e730c99dc7deec4c68ac045118c12fc3fa141
JangYe <1481660657@qq.com>
2017-11-02 14:09:47 +0800

    First
    The first version

---
+++ Opera.java
@@ -0,0 +1,17 @@
+package java_demo02;
+
+public class Opera {
+    public static double opera(double a, char theta, double b){
+        switch (theta){
+            case '+':
+                return (new Addtion()).operation(a,b);
+            case '-':
+                return (new Subtraction()).operation(a,b);
+            case '*':
+                return (new Multiplication()).operation(a,b);
+            case '/':
+                return (new Division()).operation(a,b);
+        }
+        return 0;
+    }
+}
---
+++ TestCalc.java
@@ -0,0 +1,10 @@
+package java_demo02;
+
+import java.io.DataInputStream;
+import java.io.IOException;
+
+public class TestCalc {
+    public static void main(String[] args) throws IOException {
+        System.out.print(Tool.EvaluateExpress());
+    }
+}
